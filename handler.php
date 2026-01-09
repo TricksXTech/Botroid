7a214c8eb3837ba395b3221f613ba6f9f02ddc6a
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2026-01-10 01:33:34 +0530

    Update handler.php

--- handler.php
+++ handler.php
@@ -23,4 +23,90 @@ function broadcastDocs($docsURL,$message,$mode){
 function loader($userid){
   //here
 }
+
+function custom_charset() {
+    return 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+}
+
+function generate_shuffle($key) {
+    $base = custom_charset();
+    $chars = str_split($base);
+
+    $hash = hash('sha256', $key, true);
+    $seed = unpack('N', substr($hash, 0, 4))[1];
+
+    mt_srand($seed);
+    shuffle($chars);
+    mt_srand(); // reset randomness
+
+    return implode('', $chars);
+}
+
+function inc_encode($input, $key) {
+    $base = custom_charset();
+    $shuffle = generate_shuffle($key);
+
+    $map = array_combine(str_split($base), str_split($shuffle));
+
+    $out = '';
+    foreach (str_split($input) as $ch) {
+        if (!isset($map[$ch])) {
+            throw new Exception("Invalid character detected");
+        }
+        $out .= $map[$ch];
+    }
+
+    return $out;
+}
+
+function inc_decode($encoded, $key) {
+    $base = custom_charset();
+    $shuffle = generate_shuffle($key);
+
+    $map = array_combine(str_split($shuffle), str_split($base));
+
+    $out = '';
+    foreach (str_split($encoded) as $ch) {
+        if (!isset($map[$ch])) {
+            throw new Exception("Invalid character detected");
+        }
+        $out .= $map[$ch];
+    }
+
+    return $out;
+}
+
+function inc_encodeV2($input, $key="KeyX") {
+    $base = custom_charset();
+    $shuffle = generate_shuffle($key);
+
+    $map = array_combine(str_split($base), str_split($shuffle));
+
+    $out = '';
+    foreach (str_split($input) as $ch) {
+        if (!isset($map[$ch])) {
+            throw new Exception("Invalid character detected");
+        }
+        $out .= $map[$ch];
+    }
+
+    return $out;
+}
+
+function inc_decodeV2($encoded, $key="KeyX") {
+    $base = custom_charset();
+    $shuffle = generate_shuffle($key);
+
+    $map = array_combine(str_split($shuffle), str_split($base));
+
+    $out = '';
+    foreach (str_split($encoded) as $ch) {
+        if (!isset($map[$ch])) {
+            throw new Exception("Invalid character detected");
+        }
+        $out .= $map[$ch];
+    }
+
+    return $out;
+}
 ?>
